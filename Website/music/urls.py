67b03ac2b3babc9f9e4585cef8e9c8b68614ef2a
pakota <pakota@cisoc.com>
2018-10-25 22:09:11 +0530

    steps of django

--- Website/music/urls.py
+++ Website/music/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.urls import re_path  # for higher versions..
 
 urlpatterns = [
 
@@ -9,6 +10,16 @@ urlpatterns = [
     # /music/app_name
     path('app_name',views.app_name,name='app_name'),
 
+
+    # for lower versions of django 2.0, its re to be used..
+    # for higher versions of django 2.2 and above, it should be used as re_path
+
     # /music/123/
-    path('(?P<alb_id>[0-9]+)',views.details,name='details')
+
+    re_path('(?P<alb_id>[0-9]+)',views.detail,name='detail')    # this also works..
+    #path(r'^[0-9]$',views.detail,name='detail')
+    #re_path('<alb_id>(\d+)/', views.detail, name='detail')
+    #path('<str:alb_id>', views.detail, name='detail')  # /music/71/
+    #re_path(r'^(?P<alb_id>[0-9]+)/$', views.detail, name='detail') # this also works..
+
 ]
--- Website/music/views.py
+++ Website/music/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album, Song
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>This is 1st page")
+    albums = Album.objects.all()
+    html =''
+    for album in albums:
+        url =  str(album.id) +'/'
+        html+= ' <a href="'+ url +'">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
 
 def app_name(request):
     return HttpResponse("<h1>This is Music App")
 
-def details(request, alb_id):
-    return HttpResponse("<h1>The Details of album : " +str(alb_id) + " are below: </h1>")
+def detail(request, alb_id):
+    return HttpResponse("<h1>The Details of album : " +str(alb_id) + " are below </h1>")
