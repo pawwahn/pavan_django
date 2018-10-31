183e3bee23814df4494c7578853cf8c7014bf872
pakota <pakota@cisoc.com>
2018-10-31 11:18:41 +0530

    steps of django

--- Website/music/views.py
+++ Website/music/views.py
@@ -15,24 +15,41 @@ from .models import Album, Song
 #     return HttpResponse(html)
 
 
+# def index(request):
+#     # getting values from db
+#     all_albums = Album.objects.all()
+#
+#     # create a folder under music>templates>music> and create ur html files here
+#     # with the help of loader.get_template() , django understands from whr to get the file..
+#
+#     #create a dictonary(which can be with any name(preferable 'context'))
+#     context = {'all_albums':all_albums}
+#
+#     template = loader.get_template('music/index.html')
+#     return HttpResponse(template.render(context, request)) # template.render(dict, request) -- every view return something.. # can be done like this and also we have other way..
+
+
+
 def index(request):
     # getting values from db
     all_albums = Album.objects.all()
-
     # create a folder under music>templates>music> and create ur html files here
-    # with the help of loader.get_template() , django understands from whr to get the file..
 
     #create a dictonary(which can be with any name(preferable 'context'))
     context = {'all_albums':all_albums}
-
-    ##template = loader.get_template('music/index.html')
-    ##return HttpResponse(template.render(context, request)) # template.render(dict, request) -- every view return something.. # can be done like this and also we have other way..
-
     return render(request, 'music/index.html',context)
 
 
 def app_name(request):
     return HttpResponse("<h1>This is Music App")
 
+# def detail(request, alb_id):
+#     return HttpResponse("<h1>The Details of album : " +str(alb_id) + " are below </h1>")
+
 def detail(request, alb_id):
-    return HttpResponse("<h1>The Details of album : " +str(alb_id) + " are below </h1>")
+    try:
+        album = Album.objects.get(pk=alb_id)
+        context = {'album':album}
+    except Album.DoesNotExist:
+        raise Http404("Album Does Not Exist")
+    return render(request, 'music/detail.html', context)
